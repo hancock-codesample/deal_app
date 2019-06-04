945a8e6900399ce8363706b44dc9fd592ba6a9ea
d-hancock <40437578+d-hancock@users.noreply.github.com>
2019-06-04 13:08:44 -0500

    first commit

--- deal_app/properties/api/urls.py
+++ deal_app/properties/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from deal_app.properties.api.views import PropertyListCreateAPIView, PropertyRetrieveUpdateDestroyAPIView
+from deal_app.properties.api.views import PropertyListCreateAPIView, PropertyRetrieveUpdateDestroyAPIView, \
+    PropertyIncomeCreateAPIView, PropertyIncomeRetrieveUpdateDestroyAPIView, PropertyPurchaseCreateAPIView, \
+    PropertyPurchaseRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path(
@@ -8,27 +10,28 @@ urlpatterns = [
         name='property-create-api'
     ),
     path(
-        'api/<int:uuid>-property',
+        'api/<int:uuid>/property/',
         view=PropertyRetrieveUpdateDestroyAPIView.as_view(),
         name='property-rud-api'
-    )
-    path(
-        '/',
-        view=PropertyListCreateAPIView.as_view(),
-        name='property-create-api'
     ),
     path(
-        'api/<int:uuid>-property',
-        view=PropertyRetrieveUpdateDestroyAPIView.as_view(),
-        name='property-rud-api'
-    ) path(
-        '/',
-        view=PropertyListCreateAPIView.as_view(),
-        name='property-create-api'
+        'api/property-income/',
+        view=PropertyIncomeCreateAPIView.as_view(),
+        name='property-income-create-api'
     ),
     path(
-        'api/<int:uuid>-property',
-        view=PropertyRetrieveUpdateDestroyAPIView.as_view(),
-        name='property-rud-api'
+        'api/<int:uuid>/property-income/',
+        view=PropertyIncomeRetrieveUpdateDestroyAPIView.as_view(),
+        name='property-income-rud-api'
+    ),
+    path(
+        'api/property-purchase/',
+        view=PropertyPurchaseCreateAPIView.as_view(),
+        name='property-purchase-create-api'
+    ),
+    path(
+        'api/<int:uuid>/property-purchase/',
+        view=PropertyPurchaseRetrieveUpdateDestroyAPIView.as_view(),
+        name='property-purchase-rud-api'
     )
 ]
